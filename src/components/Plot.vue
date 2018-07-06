de79eb0d37e1554cd5e68368b1c44a98affaf8d7
Mike Roy <mikevroy@gmail.com>
2018-07-06 10:28:34 -0400

    Adds initial functionality

--- src/components/Plot.vue
+++ src/components/Plot.vue
@@ -1,5 +1,4 @@
 <template>
-
     <GridLayout columns="*, *">
         <StackLayout col="0">
             <TextField  v-model="entity.project" hint="Project/Site" />
@@ -7,30 +6,54 @@
             <TextField  v-model="entity.applicant" hint="Applicant/Owner" />
             <TextField  v-model="entity.state" hint="State" />
             <TextField  v-model="entity.samplingPoint" hint="Sampling Point" />
-        </StackLayout>
-        <StackLayout col="1">
-            <Label text="Sampling Date" />
-            <DatePicker v-model="entity.samplingDate" height="60" />
             <TextField  v-model="entity.investigators" hint="Investigators" />
             <TextField  v-model="entity.township" hint="Section, Township, Range" />
             <TextField  v-model="entity.landform" hint="Landform" />
             <TextField  v-model="entity.localRelief" hint="Local relief" />
             <TextField  v-model="entity.slope" hint="Slope (%)" />
+            <TextField  v-model="entity.soilMap" hint="Soil Map Unit Name" />
+            <TextField  v-model="entity.nwi" hint="NWI Classification" />
+
+            <StackLayout orientation="horizontal">
+                <Switch v-model="entity.climatic" />
+                <Label text="Climatic / Hydrologic Conditions" />
+            </StackLayout>
+
+        </StackLayout>
+        <StackLayout col="1">
+            <Label text="Sampling Date" />
+            <DatePicker v-model="entity.samplingDate" height="60" />
+
+            <StackLayout>
+                <Button text="Get location" @tap="enableLocationServices" :visibility="currentGeoLocation.latitude ? 'collapsed' : 'visible'" />
+                <StackLayout :visibility="currentGeoLocation.latitude ? 'visible' : 'collapsed'">
+                    <Label :text="'Latitude: ' + currentGeoLocation.latitude" />
+                    <Label :text="'Longitude: ' + currentGeoLocation.longitude" />
+                </StackLayout>
+            </StackLayout>
 
             <Label text="Subregion" />
             <ListPicker :items="listOfSubregions"
                         v-model="entity.subregion"
                         height="60"
                         width="120"
-                        horizontalAlignment="left"
-                        @selectedIndexChange="selectedIndexChanged" />
+                        horizontalAlignment="left"/>
 
-            <Button text="Button" @tap="onButtonTap" />
+            <Label text="Datum" />
+            <ListPicker :items="listOfDatum"
+                        v-model="entity.datum"
+                        height="60"
+                        width="120"
+                        horizontalAlignment="left"/>
+
         </StackLayout>
     </GridLayout>
 </template>
 
 <script>
+    import * as geolocation from 'nativescript-geolocation';
+    import * as listviewModule from 'nativescript-ui-listview';
+
     export default {
         data () {
             return {
@@ -47,17 +70,39 @@
                     localRelief: '',
                     slope: '',
                     subregion: '',
+                    climatic: false
                 },
                 listOfSubregions: ["LRR", "MLRA"],
+                listOfDatum: ["WGS84", "NAD83"],
+                currentGeoLocation: {
+                    latitude: null,
+                    longitude: null,
+                    altitude: null,
+                    direction: null
+                },
             }
         },
         methods: {
-            onButtonTap () {
-                console.log(this.entity)
+            enableLocationServices () {
+                geolocation.isEnabled().then(enabled => {
+                    if (!enabled) {
+                        geolocation.enableLocationRequest().then(() => this.showLocation());
+                    } else {
+                        this.showLocation();
+                    }
+                });
             },
-            selectedIndexChanged() {
-                console.log('Changed subregion')
-            }
+            showLocation () {
+                geolocation.watchLocation(location => {
+                    this.currentGeoLocation = location;
+                }, error => {
+                    alert(error);
+                }, {
+                    desiredAccuracy: 3,
+                    updateDistance: 10,
+                    minimumUpdateTime: 1000 * 1
+                });
+            },
         }
     };
 </script>
